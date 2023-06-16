330cce44d3c1c3355300a302362ed8fc6e9215e9
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-06-16 15:39:14 +0200

    connect, download db

--- R/connect_to_db.R
+++ R/connect_to_db.R
@@ -1,3 +1,18 @@
+#' Connect to a SQLite database
+#'
+#' This function establishes a connection to a SQLite database file located at the specified path using the DBI and RSQLite packages.
+#'
+#' @param path_to_db The path to the SQLite database file.
+#'
+#' @return A database connection object.
+#' @export
+#'
+#' @examples
+#' # Connect to a SQLite database file named "mydb.db" in the current working directory
+#' connection <- connect_to_db("mydb.db")
+#'
+#' @import DBI
+#' @import RSQLite
 connect_to_db <- function(path_to_db){
   conn = DBI::dbConnect(RSQLite::SQLite(), path_to_db)
   return(conn)
--- R/download_db.R
+++ R/download_db.R
@@ -1,8 +1,21 @@
+#' Download the database file
+#'
+#' This function downloads the database file from a specified URL and saves it to the local path.
+#'
+#' @param local_path The local path where the downloaded file will be saved.
+#'
+#' @return NULL (no explicit return value).
+#' @export
+#'
+#' @examples
+#' # Download the database file to the current working directory
+#' download_db("initial_db.db")
+#'
 download_db <- function(local_path){
   # TODO: Have this updated
   # TODO: Return a "last updated" file somewhere.
   db_url = "https://github.com/jstbcs/inhibitiontasks/raw/inhibitiontaks_db2023/initial_db.db"
-  download.file(
+  utils::download.file(
     url = db_url,
     destfile = local_path,
     quiet = TRUE
