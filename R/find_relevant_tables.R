3c5f9c4b34529163988a19d612fb2b372f38fb04
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-06-29 10:10:05 +0200

    Update find_relevant_tables.R

--- R/find_relevant_tables.R
+++ R/find_relevant_tables.R
@@ -25,7 +25,8 @@ find_relevant_tables <- function(conn, column_name, info = NULL) {
   tables <- info[info$column == column_name, "table"]
 
   if (length(tables) == 0) {
-    stop("This column does not exist in any table in this database")
+    msg = paste("The column:", column_name, "does not exist in any table in this database")
+    stop(msg)
   }
 
   return(tables)
