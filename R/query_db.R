a3a1ce3a0fb32d3089d41a026724c8d9e86ba5c6
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-08-31 12:33:54 -0500

    query_targeted now default

--- R/query_db.R
+++ R/query_db.R
@@ -1,77 +1,64 @@
-#' Query the database based on specified arguments
+#' Query Database
 #'
-#' This function queries the database based on the provided arguments and returns the requested data.
-#' It matches primary keys from the initial queries and finds connected keys based on the argument sequence.
-#' The data is then retrieved from the database using the obtained IDs.
+#' This function performs targeted queries on a database using specified filtering arguments and returns the query results.
 #'
-#' @param conn A database connection object.
-#' @param arguments A list of SQL queries specifying the arguments.
-#'   Each query should retrieve the primary keys for matching records.
-#' @param argument_relation The relation between the arguments. Defaults to "and".
-#'   Possible values include "and" and "or" and a custom vector.
+#' @param conn The connection object or database connection string.
+#' @param arguments A list of filtering arguments for the query.
+#' @param target_vars A character vector specifying the variables to be included in the query results.
+#' @param argument_relation A character string specifying the relation between filtering arguments ("and" or "or").
+#' @param target_table The target table in the database for querying.
+#' @param full_db A logical value indicating whether to return the entire database. Only works when the target table is "observation_table".
 #'
-#' @return The requested data from the database based on the specified arguments.
+#' @return The query results as a data frame.
 #' @export
-#' @examples
-#' # Query the database
-#' conn <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
-#' arguments <- list(
-#'   "SELECT observation_id FROM observation_table WHERE value > 10",
-#'   "SELECT between_id FROM between_table WHERE between_var = 'A'"
-#' )
-#' query_db(conn, arguments)
-#'
-query_db <- function(conn, arguments, argument_relation = "and"){
-  # Querying starts
-  column_names = get_column_names(conn)
-  argument_matches = list()
+query_db <- function(conn, arguments, target_vars = NULL, argument_relation = "and", target_table = "observation_table", full_db = TRUE){
+  if (full_db == TRUE & target_table != "observation_table"){
+    warning("Requesting return of full database only works with the observation_table as the target table. (This is due to poor implementation of the backward join logic)")
+  }
 
-  argument_sequence = get_argument_sequence(arguments, argument_relation)
-
-  for (i in seq_along(arguments)){
-    # Loop through arguments, find matching primary keys
-    # Then find keys connected to these matches
-    # If its the first iteration (first argument), then the init ids is the matches
-    # Otherwise it is only the matches that are already present in the arguments
-    if (i == 1){
-      # Find out which table is queried from
-      table = which_table_is_queried(arguments[[i]])
-
-      # Obtain the ids that match that query
-      matching_ids = DBI::dbGetQuery(conn, arguments[[i]])[[return_id_name_from_table(table)]]
-
-      # Return ids connected to this into the appropriate sublist in argument_matches
-      argument_matches[[i]] = return_connected_ids(conn, table, matching_ids)
-
-    } else {
-      if (argument_sequence[i] != argument_sequence[i - 1]){
-        # then it is an "and" connector, you should have starting ids
-        table = which_table_is_queried(arguments[[i]])
-
-        init_ids = argument_matches[[i - 1]][[table]]
-
-        matching_ids = DBI::dbGetQuery(conn, arguments[[i]])[[return_id_name_from_table(table)]]
-
-        relevant_ids = matching_ids[matching_ids %in% init_ids]
-
-        argument_matches[[i]] = return_connected_ids(conn, table, relevant_ids)
-
-      } else {
-        # then it is an "or" connector, you should return all ids
-        table = which_table_is_queried(arguments[[i]])
-
-        matching_ids = DBI::dbGetQuery(conn, arguments[[i]])[[return_id_name_from_table(table)]]
-
-        argument_matches[[i]] = return_connected_ids(conn, table, matching_ids)
-      }
+  # When only requesting a specific table, make sure that target vars are present in that table
+  if (full_db == FALSE){
+    table_fields = DBI::dbListFields(conn, target_table)
+    target_vars = target_vars[target_vars %in% table_fields]
+    if (length(target_vars) == 0){
+      warning("None of the variables you specified are present in the target table. Returning all variables from the target table.")
+      target_vars = NULL
     }
   }
 
-  # Now use the list of argument matches to return a proper list depending on structure
-  proper_matches = return_proper_ids(argument_matches, argument_sequence)
-  # Then use that list to query the db
+  argument_matches = vector(mode = "list", length = length(arguments))
 
-  read_data = return_entries_from_id(conn, proper_matches)
+  argument_sequence = get_argument_sequence(arguments, argument_relation)
 
-  return(read_data)
+  path_list = compute_fastest_way_to_table(conn, target_table = target_table)
+  target_table_id = return_id_name_from_table(target_table)
+
+  if (full_db == FALSE){
+    data = dbGetQuery(
+      conn,
+      combine_sql_queries(
+        arguments,
+        argument_sequence,
+        path_list,
+        target_vars
+      )
+    )
+  } else {
+    data = dbGetQuery(
+      conn,
+      add_join_paths_to_query(
+        conn,
+        combine_sql_queries(
+          arguments,
+          argument_sequence,
+          path_list,
+          target_vars
+        ),
+        precompute_table_join_paths(conn),
+        target_vars
+      )
+    )
+  }
+
+  return(data)
 }
---
+++ R/query_db_deprecated.R
@@ -0,0 +1,77 @@
+#' Query the database based on specified arguments
+#'
+#' This function queries the database based on the provided arguments and returns the requested data.
+#' It matches primary keys from the initial queries and finds connected keys based on the argument sequence.
+#' The data is then retrieved from the database using the obtained IDs.
+#'
+#' @param conn A database connection object.
+#' @param arguments A list of SQL queries specifying the arguments.
+#'   Each query should retrieve the primary keys for matching records.
+#' @param argument_relation The relation between the arguments. Defaults to "and".
+#'   Possible values include "and" and "or" and a custom vector.
+#'
+#' @return The requested data from the database based on the specified arguments.
+#' @export
+#' @examples
+#' # Query the database
+#' conn <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+#' arguments <- list(
+#'   "SELECT observation_id FROM observation_table WHERE value > 10",
+#'   "SELECT between_id FROM between_table WHERE between_var = 'A'"
+#' )
+#' query_db(conn, arguments)
+#'
+query_db_deprecated <- function(conn, arguments, argument_relation = "and"){
+  # Querying starts
+  column_names = get_column_names(conn)
+  argument_matches = list()
+
+  argument_sequence = get_argument_sequence(arguments, argument_relation)
+
+  for (i in seq_along(arguments)){
+    # Loop through arguments, find matching primary keys
+    # Then find keys connected to these matches
+    # If its the first iteration (first argument), then the init ids is the matches
+    # Otherwise it is only the matches that are already present in the arguments
+    if (i == 1){
+      # Find out which table is queried from
+      table = which_table_is_queried(arguments[[i]])
+
+      # Obtain the ids that match that query
+      matching_ids = DBI::dbGetQuery(conn, arguments[[i]])[[return_id_name_from_table(table)]]
+
+      # Return ids connected to this into the appropriate sublist in argument_matches
+      argument_matches[[i]] = return_connected_ids(conn, table, matching_ids)
+
+    } else {
+      if (argument_sequence[i] != argument_sequence[i - 1]){
+        # then it is an "and" connector, you should have starting ids
+        table = which_table_is_queried(arguments[[i]])
+
+        init_ids = argument_matches[[i - 1]][[table]]
+
+        matching_ids = DBI::dbGetQuery(conn, arguments[[i]])[[return_id_name_from_table(table)]]
+
+        relevant_ids = matching_ids[matching_ids %in% init_ids]
+
+        argument_matches[[i]] = return_connected_ids(conn, table, relevant_ids)
+
+      } else {
+        # then it is an "or" connector, you should return all ids
+        table = which_table_is_queried(arguments[[i]])
+
+        matching_ids = DBI::dbGetQuery(conn, arguments[[i]])[[return_id_name_from_table(table)]]
+
+        argument_matches[[i]] = return_connected_ids(conn, table, matching_ids)
+      }
+    }
+  }
+
+  # Now use the list of argument matches to return a proper list depending on structure
+  proper_matches = return_proper_ids(argument_matches, argument_sequence)
+  # Then use that list to query the db
+
+  read_data = return_entries_from_id(conn, proper_matches)
+
+  return(read_data)
+}
