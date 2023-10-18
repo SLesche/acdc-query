1eb8c38e4c66b22dd190286360f98f48d1e3715f
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2023-10-18 10:24:55 +0200

    remove acdc db from package

--- R/add_argument.R
+++ R/add_argument.R
@@ -14,8 +14,19 @@
 #' @return A list object with the new argument (SQL query) added.
 #' @export
 #' @examples
-#' db_file <- base::system.file("extdata", "acdc.db", package = "acdcquery")
-#' conn <- connect_to_db(db_file)
+#'
+#' conn <- connect_to_db(":memory:")
+#'
+#' mtcars$mtcars_id = 1:nrow(mtcars)
+#'
+#' example_data = data.frame(
+#'   example_id = 1:150,
+#'   mtcars_id = rep(1:30, each = 5),
+#'   example_value = runif(150, 0, 1)
+#' )
+#'
+#' DBI::dbWriteTable(conn, "mtcars_table", mtcars)
+#' DBI::dbWriteTable(conn, "example_table", example_data)
 #'
 #' # Initializing argument list
 #' arguments = list()
@@ -24,34 +35,34 @@
 #' arguments = add_argument(
 #'  list = arguments,
 #'  conn = conn,
-#'  variable = "task_name",
+#'  variable = "cyl",
 #'  operator = "equal",
-#'  values = c("stroop", "flanker")
+#'  values = c(4, 6)
 #' )
 #'
 #' # Using "greater" operator
 #' arguments = add_argument(
 #'  list = arguments,
 #'  conn = conn,
-#'  variable = "n_participants",
+#'  variable = "cyl",
 #'  operator = "greater",
-#'  values = 100
+#'  values = 2
 #' )
 #'
 #' # Using "between" operator
 #' arguments = add_argument(
 #'  list = arguments,
 #'  conn = conn,
-#'  variable = "percentage_congruent",
+#'  variable = "cyl",
 #'  operator = "between",
-#'  values = c(0.2, 0.4)
+#'  values = c(2, 8)
 #' )
 #'
 #' # Manully constructing a filter statement
 #' manual_arguments = add_argument(
 #'  list = arguments,
 #'  conn = conn,
-#'  statement = "SELECT condition_id FROM condition_table WHERE n_obs > 100)"
+#'  statement = "SELECT mtcars_id FROM mtcars WHERE cyl = 4 OR cyl = 6)"
 #' )
 
 add_argument <- function(list, conn, variable, operator, values, statement = NULL) {
